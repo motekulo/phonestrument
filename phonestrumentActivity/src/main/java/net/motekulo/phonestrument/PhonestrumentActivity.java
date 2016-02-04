980c164c0d2b441a58369d41bfd0c15a1400d896
Denis Crowdy <motekulo@gmail.com>
2016-02-04 12:41:41 +1100

    Get old pd patch and app working but with phonestrument name

--- phonestrumentActivity/src/main/java/net/motekulo/phonestrument/PhonestrumentActivity.java
+++ phonestrumentActivity/src/main/java/net/motekulo/phonestrument/PhonestrumentActivity.java
@@ -28,7 +28,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
 import android.content.SharedPreferences;
-import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.os.IBinder;
