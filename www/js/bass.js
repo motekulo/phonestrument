1080fc39e9c78fdd9c81af8da33ae689b6e15261
Denis Crowdy <motekulo@gmail.com>
2016-04-30 10:45:09 -0700

    Add octave up and down buttons and clean up gui

--- www/js/bass.js
+++ www/js/bass.js
@@ -18,20 +18,57 @@ function bass() {
     // dampening:4000
     // resonance:0.9
 
-    var note = ["C2", "D2", "E2", "F2", "G2", "A2", "B2"];
+    //var note = ["C2", "D2", "E2", "F2", "G2", "A2", "B2"];
+    var note = [26, 28, 30, 31, 33, 35, 37];
 
     // Interface ////////////////////////////////////////////
 
-
     var a = new Interface.Panel({ 
         container:document.querySelector("#InterfacePanel") 
+    });
+
+    var ub = new Interface.ButtonV({
+        bounds: [.05, .1, .1, .1],
+        points: [{x:.25,y:1},{x:.75,y:1},{x:.75,y:.5},{x:1,y:.5},{x:.5,y:0},
+        {x:0,y:.5},{x:.25,y:.5}],
+        mode: 'contact', 
+        label: '8ve',
+        textLocation: {x:.5,y:.5},
+        onvaluechange : function (){
+            console.log("8ve up");
+            if (note[0] < 96) {
+                for (i=0; i < note.length; i++){
+                    note[i] = note[i] + 12;
+                }
+
+            }
+        }
+    });
+
+    var db = new Interface.ButtonV({
+        bounds: [.05, .7, .1, .1],
+        points: [{x:.25,y:0},{x:.75,y:0},{x:.75,y:.5},{x:1,y:0.5},{x:.5,y:1},
+        {x:0,y:.5},{x:.25,y:.5}],
+        mode: 'contact', 
+        label: '8ve',
+        textLocation: {x:.5,y:.5},
+        onvaluechange : function (){
+            console.log("8ve down");
+            if (note[0] > 12) {
+                for (i=0; i < note.length; i++){
+                    note[i] = note[i] - 12;
+                }
+
+            }
+        }
+
     });
 
     var mb = new Interface.MultiButton({
         rows: 7, 
         columns: 1,
         mode: 'contact',  // has no effect
-        bounds:[.05, .05, .5, .8],
+        bounds:[.2, .05, .45, .8],
         onvaluechange : function(row, col, value) {
             console.log( 'row : ' + row + ' , col : ' + col + ' , value : ' + value);
             timestring = "0:0:" + col;
@@ -51,7 +88,7 @@ function bass() {
         min: -36,
         max: 6,
         label: 'Vol',
-        bounds: [0.65, 0.05, 0.15, 0.25],
+        bounds: [0.7, 0.05, 0.1, 0.25],
         onvaluechange : function() {
             pluckbass.volume.value = this.value;
             // console.log("this.value " + this.value);
@@ -64,7 +101,7 @@ function bass() {
         min: 0,
         max: 1,
         label: 'Attack',
-        bounds: [0.85, 0.05, 0.15, 0.25],
+        bounds: [0.85, 0.05, 0.1, 0.25],
         onvaluechange : function() {
             pluckbass.attackNoise.value = this.value;
             // console.log("this.value " + this.value);
@@ -77,7 +114,7 @@ function bass() {
         min: 0,
         max: 5000,
         label: 'Dampening',
-        bounds: [0.65, 0.35, 0.15, 0.25],
+        bounds: [0.7, 0.35, 0.1, 0.25],
         onvaluechange : function() {
             pluckbass.dampening.value = this.value;
             // console.log("this.value " + this.value);
@@ -90,7 +127,7 @@ function bass() {
         min: 0,
         max: 1,
         label: 'Resonance',
-        bounds: [0.85, 0.35, 0.15, 0.25],
+        bounds: [0.85, 0.35, 0.1, 0.25],
         onvaluechange : function() {
             pluckbass.resonance.value = this.value;
             // console.log("this.value " + this.value);
@@ -99,7 +136,7 @@ function bass() {
 
 
 
-    a.add(mb, vs, as, ds, rs);
+    a.add(ub, db, mb, vs, as, ds, rs);
 
 
 
