a1cd9ed6f66b05be7c8d845a6274b6ec31ff1b39
Denis Crowdy <motekulo@gmail.com>
2016-05-11 15:56:42 +1000

    Initial version of adaptor
    Using instance variables for converted data; should maybe return the
    converted data to the sequencer, then the sequencer can control sending
    it to the part via the adaptor?

--- spec/simplebarseqadaptor.js
+++ spec/simplebarseqadaptor.js
@@ -3,18 +3,22 @@ describe("Simple bar sequencer adaptor", function() {
 
     beforeEach(function() {
         adaptor = new SimpleBarSequencerAdaptor();
+        part = new Part();
+        adaptor.connectToPart(part);
     });
 
     it("should be able to be connected to a part", function() {
-        part = new Part();
-        adaptor.connectToPart(part);
-        expect(adaptor.part).toBeDefined;
+        expect(adaptor.part).toBeDefined();
 
     });
 
     it("should be able to receive data to convert ", function() {
         var data = [0, 1, 1]  // simple bar seq sends row, col, val
-        expect(adaptor.setDataToConvert(data)).toBeDefined;
+        adaptor.convertData(data);
+        expect(adaptor.index).toBe(0);
+        expect(adaptor.time).toBe("1 * 16n");
+        expect(adaptor.note).toBe("C4");
+
     });
 
 
--- www/js/simplebarseqadaptor.js
+++ www/js/simplebarseqadaptor.js
@@ -1,4 +1,17 @@
 function SimpleBarSequencerAdaptor() {
+    var octave = 4; // fudge for now
+    var pitch = ["C","D","E","F"]; // fudge it for now
+
+    // converts from:
+    // Array of data in row, column, value form
+    //
+    // converts to:
+    // voice index, time (eg "1 * 4s"), pitch (eg "C4)
+    //
+
+    this.index = null;
+    this.time = "";
+    this.note = "";
 
     this.part = null;
 
@@ -7,15 +20,36 @@ function SimpleBarSequencerAdaptor() {
 
     }
 
-    this.setDataToConvert = function(data) {
-/* From a simple bar sequencer we get row, column, value data
+    this.convertData = function(data) {
+        /* From a simple bar sequencer we get row, column, value data
 
- A part expects a voice index (row, in this case), time, and pitch
+           A part expects a voice index (row, in this case), time, and pitch
 
 
 
 */
+        if (data.length >= 3) {
+            var row = data[0];
+            var col = data[1];
+            var val = data[2];
+
+            this.index = row;
+            this.time = col + " * 16n";
+            if (val == 1) {
+                this.note = pitch[row] + octave;
+            } else {
+                this.note = null;
+            }
+//        this.part.setNoteArray(row, time. note);
+        return 0;
+        }
+
     }
+
+    this.sendConvertedDataToPart = function(index, time, note) {
+        this.part.setNoteArray(index, time, note);
+    }
+
 
 }
 
