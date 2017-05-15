8bbb04073b1144ebbe162649c18700ca09495397
Denis Crowdy <dcrowdy@gmail.com>
2017-05-15 17:33:14 +1000

    Sort out some chord progression changes

--- www/app.js
+++ www/app.js
@@ -14,6 +14,7 @@ var bubbles;
 
 var tonalEnv;
 var notes = [];
+var chordProgPart;
 
 plucky = new Tone.PolySynth(3, Tone.MonoSynth);
 pluckyPanVol = new Tone.PanVol(0.5, -24);
@@ -47,7 +48,7 @@ function create () {
     var allNotes = tonalEnv.getFullChordArray(1, 7, [0,0,0,-1]);
     notes = tonalEnv.trimArray(allNotes, 36, 84);
 
-    for (var i = 0; i < 3; i++) {
+    for (var i = 0; i < 6; i++) {
         musBubble = bubbles.create(game.world.randomX, game.world.randomY, 'bubble');
         musBubble.anchor.set(0.5, 0.5);
         musBubble.inputEnabled = true;
@@ -61,8 +62,8 @@ function create () {
         musBubble.checkWorldBounds = true;
         musBubble.body.collideWorldBounds = true;
 
-        musBubble.body.velocity.x = game.rnd.between(-100, 100);
-        musBubble.body.velocity.y = game.rnd.between(-100, 100);
+        musBubble.body.velocity.x = game.rnd.between(-200, 200);
+        musBubble.body.velocity.y = game.rnd.between(-200, 200);
 
         musBubble.body.angularVelocity = game.rnd.between(-10, 10);
 
@@ -74,7 +75,7 @@ function create () {
             startNotes.push(noteName);
         }
 
-        var subDiv = game.rnd.pick([1, 2, 3, 4, 8, 12, 16]) + "n";
+        var subDiv = game.rnd.pick([1, 2, 3, 4, 12]) + "n";
 
         musBubble.tonePattern = new Tone.Sequence(function(time, note){
             //console.log("Note is " + note);
@@ -88,24 +89,94 @@ function create () {
 
     }
 
+    Tone.Transport.scheduleRepeat(function(time){
+           // cycle through balls and modify sequence notes based on position
+           bubbles.forEach(function(bubble){
+               var pitch = notes[Math.floor(bubble.body.y/game.world.height * notes.length)];
+               var index = game.rnd.pick([0, 1, 2, 3]);
+               bubble.tonePattern.at(index, pitch);
+           }, this, true);
+    }, "4n");
+
+    bassSynth = new Tone.MonoSynth();
+    bassPanVol = new Tone.PanVol(0.5, -24);
+    bassSynth.connect(bassPanVol);
+    bassPanVol.connect(Tone.Master);
+
+
+
+    chordProgPart = new Tone.Part(function(time, value) {
+            //console.log("chord change " + value);
+            console.log("bar num " + Tone.Transport.position);
+            var allNotes = tonalEnv.getFullChordArray(value.root, value.tochordtone, value.alterations);
+            notes = tonalEnv.trimArray(allNotes, 36, 84);
+            bassArpeggio = tonalEnv.scaleOctave(tonalEnv.getChord(value.root,
+                                                                  value.tochordtone, value.alterations), 3);
+            for (var i = 0; i < bassArpeggio.length; i++) {
+                //var time = "0:" + i;
+                bassPart.at(i, bassArpeggio[i]);
+            }
+        }, [
+            {time: "0m",
+            root: 1,
+            tochordtone: 7,
+            alterations: [0,0,0,-1]},
+            {time: "1m",
+            root: 4,
+            tochordtone: 7,
+            alterations: [0,0,0,-1]},
+            {time: "2m",
+            root: 1,
+            tochordtone: 7,
+            alterations: [0,0,0,-1]},
+            {time: "4m",
+            root: 4,
+            tochordtone: 7,
+            alterations: [0,0,0,-1]},
+            {time: "6m",
+            root: 1,
+            tochordtone: 7,
+            alterations: [0,0,0,-1]},
+            {time: "8m",
+            root: 2,
+            tochordtone: 7,
+            alterations: [0,0,0,0]},
+            {time: "9m",
+            root: 5,
+            tochordtone: 7,
+            alterations: [0,0,0,0]},
+            {time: "10m",
+            root: 1,
+            tochordtone: 7,
+            alterations: [0,0,0,-1]},
+            {time: "11m",
+            root: 5,
+            tochordtone: 7,
+            alterations: [0,0,0,0]}
+
+        ]);
+        chordProgPart.loop = true;
+        chordProgPart.loopEnd = "12m";
+        chordProgPart.start(0);
+
+        bassArpeggio = tonalEnv.scaleOctave(tonalEnv.getChord(1, 7, []), 4);
+
+    var bassPart = new Tone.Sequence(function(time, note){
+    	//console.log(note);
+        bassSynth.triggerAttackRelease(Tone.Frequency(note, "midi"), "8n", time);
+
+    }, bassArpeggio, "4n");
+    bassPart.loop = true;
+    bassPart.loopEnd = "1m";
+    bassPart.start(0);
+
     Tone.Transport.loop = true;
     Tone.Transport.loopStart = 0;
     Tone.Transport.loopEnd = "12m";
     Tone.Transport.bpm.value = 112;
     Tone.Transport.latencyHint = 'playback';
     Tone.Transport.start("+0.8");
-    // for (var i = 0; i < 40; i++)
-    // {
-    //     var sprite = game.add.sprite(-100 + (game.world.randomX), 600, 'bubble');
-    //
-    //     sprite.scale.set(game.rnd.realInRange(0.1, 0.6));
-    //
-    //     var speed = game.rnd.between(4000, 6000);
-    //
-    //     game.add.tween(sprite).to({ y: -256 }, speed, Phaser.Easing.Sinusoidal.InOut, true, delay, 1000, false);
-    //
-    //     delay += 200;
-    // }
+
 }
 
 function update() {
