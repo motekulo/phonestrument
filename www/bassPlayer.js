10f4fc38c6cd132ce8c188f92af9cfb459dc0eba
Denis Crowdy <dcrowdy@gmail.com>
2017-06-06 09:51:08 +1000

    Get drag working and build initial bass arpeggio

--- www/bassPlayer.js
+++ www/bassPlayer.js
@@ -1,6 +1,10 @@
 
 bassPlayer = function(game) {
     this.bubbles;  // main group of bubble sprites
+    this.bubbleScale = 0.35;
+    this.xDown;
+    this.yDown;
+    this.tonalEnv = new Tonality();
 };
 
 bassPlayer.prototype = {
@@ -22,11 +26,43 @@ bassPlayer.prototype = {
         for (var i = 0; i < 2; i++) {
             var musBubble = this.bubbles.create(game.world.randomX, game.world.randomY, 'bubble');
             musBubble.anchor.set(0.5, 0.5);
+            musBubble.inputEnabled = true;
+            musBubble.input.enableDrag(true);
+            musBubble.events.onDragStart.add(this.onDragStart, this);
+            musBubble.events.onDragStop.add(this.onDragStop, this);
+            game.physics.enable(musBubble, Phaser.Physics.ARCADE);
+
+            musBubble.scale.set(this.bubbleScale);
         }
-        //makeBubbles();
+
+
+
+        var bassArpeggio = this.tonalEnv.scaleOctave(this.tonalEnv.getChord(1, 7, []), 4);
+        var bassRoot = bassArpeggio[0];
+        bassPart = new Tone.Sequence(function(time, note){
+        	//console.log(note);
+            //bassSynth.triggerAttackRelease(Tone.Frequency(note, "midi"), "8n", time);
+
+        }, [bassRoot], "1m");
+        bassPart.loop = true;
+        bassPart.loopEnd = "1m";
+
+        
+
     },
 
     update: function() {
 
+    },
+
+    onDragStart: function(sprite, pointer) {
+        this.xDown = pointer.x;
+        this.yDown = pointer.y;
+        //console.log("touch down at " + this.xDown + ", " + this.yDown);
+    },
+
+    onDragStop: function(sprite, pointer) {
+        //console.log("touch up at " + pointer.x + ", " + pointer.y);
     }
+
 }
